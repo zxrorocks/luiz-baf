883ceefd1531b94226dbe91fa44170790c0502cd
zxrorocks <96801807+zxrorocks@users.noreply.github.com>
2024-02-14 07:46:25 -0500

    Delete auto-flipper-541231-main directory
    whoops uploaded parent folder !

--- auto-flipper-541231-main/types/fastWindowClick.d.ts
+++
@@ -1,7 +0,0 @@
-interface FastWindowClicker {
-    clickPurchase(price: number, windowId: number): void
-    clickBedPurchase(price: number, windowId: number): void
-    clickConfirm(price: number, itemName: string, windowId: number): void
-    onAuctionWasAlreadyBought(): void
-    getLastWindowId(): number
-}
